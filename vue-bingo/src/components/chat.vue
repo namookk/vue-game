05d6c54bdf1d56b56d2f0a92b3a3237844b7562d
nuhwang <nuhwang@amorepacific.com>
2021-01-07 10:42:17 +0900

    서버 변경

--- vue-bingo/src/components/chat.vue
+++ vue-bingo/src/components/chat.vue
@@ -57,7 +57,7 @@ export default {
             }
         },
         connect() {
-            const serverURL = "http://192.168.11.209:9999"
+            const serverURL = "http://54.180.210.229:9999"
             let socket = new SockJS(serverURL)
             this.stompClient = Stomp.over(socket)
             console.log("소켓 연결을 시도합니다. 서버 주소 : ${serverURL}")
