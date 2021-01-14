15aedf715b744b981aaf690ce11d04b233304f94
nuhwang <nuhwang@amorepacific.com>
2021-01-14 17:23:33 +0900

    커밋

--- vue-bingo/src/store/store.js
+++ vue-bingo/src/store/store.js
@@ -7,8 +7,8 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state: {
         login_user : '',
-        server_url : 'http://192.168.11.209:9999'
-        // server_url : 'http://54.180.210.229:9999'
+        // server_url : 'http://192.168.11.209:9999'
+        server_url : 'http://54.180.210.229:9999' 
     },
     mutations: {
         login (state,user) {
